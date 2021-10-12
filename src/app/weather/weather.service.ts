a5b4447bdf1cfeb22ccd2fdc36836f415d79e015
sheepeatsbear <pvoskou@gmail.com>
2021-10-12 16:47:11 +0100

    added material modules

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -55,11 +55,11 @@ export class WeatherService implements IWeatherService {
       country: data.sys.country,
       date: data.dt * 1000,
       image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
-      temperature: this.convertKelvinToFahrenheit(data.main.temp),
+      temperature: this.convertKelvinToCelsius(data.main.temp),
       description: data.weather[0].description,
     };
   }
-  private convertKelvinToFahrenheit(kelvin: number): number {
-    return (kelvin * 9) / 5 - 459.67;
+  private convertKelvinToCelsius(kelvin: number): number {
+    return kelvin - 273.15;
   }
 }
