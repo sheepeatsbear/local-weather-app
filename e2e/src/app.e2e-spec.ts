531c486bae619e7d697b01316627099113fcd53d
sheepeatsbear <pvoskou@gmail.com>
2021-10-12 08:46:31 +0100

    ran style:fix against code

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po'
+import { AppPage } from './app.po';
 
 describe('LocalCast Weather App', () => {
-  let page: AppPage
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new AppPage()
-  })
+    page = new AppPage();
+  });
 
   it('should display welcome message', () => {
-    page.navigateTo()
-    expect(page.getTitleText()).toEqual('LocalCast Weather')
-  })
-})
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('LocalCast Weather');
+  });
+});
